ca4c20b3f408a6454e4cb0e65189e1cf1491b1fd
Your Name <tidkepradeep@gmail.com>
2026-02-26 21:57:42 +0530

    commit from local for cicd 2nd

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # Title of the app
-st.title("🚀 My First Streamlit App  after CICD")
+st.title("🚀 My First Streamlit App  after CICD    cicd from    local git push")
 
 # Header and Text
 st.header("Welcome!")
